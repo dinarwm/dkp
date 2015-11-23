0593b936e284208ff300d4fd7f2392456c4bc28b
shoffi <shoffi.izza@gmail.com>
2015-11-23 17:00:58 +0700

    Home

---
+++ gudang/application/views/home/home.php
@@ -0,0 +1,168 @@
+          
+         
+          <ol class="breadcrumb">
+            <li><a href="<?php echo base_url() ?>access"><i class="fa fa-home"></i> Home</a></li>
+          </ol>
+          
+        </section>
+
+        	
+        <!-- Main content -->
+        <section class="content">
+        
+        	
+
+			<div>
+				
+				<h4>
+		            Statistik
+		        </h4>
+		          <hr style="border-color:#55E0DE;">
+
+		          <div class="row">
+		            <div class="col-lg-3 col-xs-6">
+		              <!-- small box -->
+		              <div class="small-box bg-red">
+		                <div class="inner">
+		                
+		                  <p>Barang Masuk</p>
+		                </div>
+		                <div class="icon">
+		                  <i class="fa fa-square"></i>
+		                </div>
+		                <a href="<?php echo base_url() ?>" class="small-box-footer">
+		                  Selengkapnya <i class="fa fa-arrow-circle-right"></i>
+		                </a>
+		              </div>
+		            </div><!-- ./col -->
+		            <div class="col-lg-3 col-xs-6">
+		              <!-- small box -->
+		              <div class="small-box bg-orange">
+		                <div class="inner">
+		                  
+		                  <p>Barang Keluar</p>
+		                </div>
+		                <div class="icon">
+		                  <i class="fa fa-bookmark"></i>
+		                </div>
+		                <a href="<?php echo base_url() ?>" class="small-box-footer">
+		                  Selengkapnya <i class="fa fa-arrow-circle-right"></i>
+		                </a>
+		              </div>
+		            </div><!-- ./col -->
+		            <div class="col-lg-3 col-xs-6">
+		              <!-- small box -->
+		              <div class="small-box bg-purple">
+		                <div class="inner">
+		                  
+		                  <p>Laporan Masuk</p>
+		                </div>
+		                <div class="icon">
+		                  <i class="fa fa-archive"></i>
+		                </div>
+		                <a href="<?php echo base_url() ?>" class="small-box-footer">
+		                  Selengkapnya <i class="fa fa-arrow-circle-right"></i>
+		                </a>
+		              </div>
+		            </div><!-- ./col -->
+		            <div class="col-lg-3 col-xs-6">
+		              <!-- small box -->
+		              <div class="small-box bg-olive">
+		                <div class="inner">
+		                  
+		                  <p></p>
+		                </div>
+		                <div class="icon">
+		                  <i class="fa fa-road"></i>
+		                </div>
+		                <a href="<?php echo base_url() ?>" class="small-box-footer">
+		                  Selengkapnya <i class="fa fa-arrow-circle-right"></i>
+		                </a>
+		              </div>
+		            </div><!-- ./col -->
+		            
+		          </div><!-- /.row -->
+
+		          
+
+		          <h4>
+		            Jalan Pintas
+		          </h4>
+		          <hr style="border-color:#55E0DE;">
+
+		        <div class="row">
+
+		        	<div class="clearfix visible-sm-block"></div>
+
+		            <?php
+		            if ($level == 1)
+		            echo '
+		            <div class="col-md-4 col-sm-6 col-xs-12">
+		              <div class="info-box">
+		              <a href="#">
+		                <span class="info-box-icon" style="background-color:#47DAB5; color:#FFFFFF"><i class="fa fa-user-plus"></i></span></a>
+		                <div class="info-box-content">
+		                  <span class="info-box-text">Pengadaan Baru</span>
+		                </div><!-- /.info-box-content -->
+		              </div><!-- /.info-box -->
+		            </div><!-- /.col -->
+		            
+
+		            <div class="col-md-4 col-sm-6 col-xs-12">
+		              <div class="info-box">
+		              <a href="#">
+		                <span class="info-box-icon bg-aqua"><i class="fa fa-square"></i></span></a>
+		                <div class="info-box-content">
+		                  <span class="info-box-text">Penyaluran Baru</span>
+		                </div><!-- /.info-box-content -->
+		              </div><!-- /.info-box -->
+		            </div><!-- /.col -->
+		            
+		            <div class="col-md-4 col-sm-6 col-xs-12">
+		              <div class="info-box">
+		              <a href="#">
+		                <span class="info-box-icon bg-green"><i class="fa fa-bookmark"></i></span></a>
+		                <div class="info-box-content">
+
+		                  <span class="info-box-text">Pelaporan Barang</span>
+		                </div><!-- /.info-box-content -->
+		              </div><!-- /.info-box -->
+		            </div><!-- /.col -->
+
+		            <div class="col-md-4 col-sm-6 col-xs-12">
+		              <div class="info-box">
+		              <a href="#">
+		                <span class="info-box-icon bg-purple"><i class="fa fa-archive"></i></span></a>
+		                <div class="info-box-content">
+
+		                  <span class="info-box-text">Cari Barang</span>
+		                </div><!-- /.info-box-content -->
+		              </div><!-- /.info-box -->
+		            </div><!-- /.col -->';
+		            ?>
+
+		            <div class="col-md-4 col-sm-6 col-xs-12">
+		              <div class="info-box">
+		              <a href="3">
+		                <span class="info-box-icon bg-red"><i class="fa fa-road"></i></span></a>
+		                <div class="info-box-content">
+		                  <span class="info-box-text">Tambah Barang</span>
+		                </div><!-- /.info-box-content -->
+		              </div><!-- /.info-box -->
+		            </div><!-- /.col -->
+		            
+		            <div class="col-md-4 col-sm-6 col-xs-12">
+		              <div class="info-box">
+		              <a href="#">
+		                <span class="info-box-icon bg-orange"><i class="fa fa-pie-chart"></i></span></a>
+		                <div class="info-box-content">
+		                  <span class="info-box-text">Tambah User</span>
+		                </div><!-- /.info-box-content -->
+		              </div><!-- /.info-box -->
+		            </div><!-- /.col 
+		    
+		          </div><!-- /.row -->
+		    </div>
+
+        </section><!-- /.content -->
+    </div>
